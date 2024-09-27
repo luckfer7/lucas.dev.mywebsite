cb5c5632bd877c05e6e5e1169373419dfc36a30a
luckfer7 <lucasferreira059@gmail.com>
2024-09-27 16:01:22 -0300

    4

--- src/Paginas/PagSkills/index.jsx
+++ src/Paginas/PagSkills/index.jsx
@@ -1,11 +1,27 @@
 
 import './index.css'
+import Csharp from './icons/c-sharp-c.svg';
+import Dotnet from './icons/dotnet.svg';
+import Css from './icons/file-type-css.svg';
+import HTML from './icons/html-5.svg';
+import JavaScript from './icons/javascript-original.svg';
+import Node from './icons/node-js.svg';
+import React from './icons/react-original-wordmark.svg';
 
 
 function MinhasSkills () {
     return (
         <>
-            
+            <h1 className='skill_titulo'>Essas são as tecnologias que domino</h1>
+            <div className='tech__container'>
+                <img className='icons__img' src={React} alt="" />
+                <img className='icons__img' src={JavaScript} alt="" />
+                <img className='icons__img' src={HTML} alt="" />
+                <img className='icons__img' src={Css} alt="" />
+                <img className='icons__img' src={Node} alt="" />
+                <img className='icons__img' src={Csharp} alt="" />
+                <img className='icons__img-net' src={Dotnet} alt="" />
+            </div>
         </>
     )
 }
--- src/Paginas/PagSobreMim/index.jsx
+++ src/Paginas/PagSobreMim/index.jsx
@@ -5,7 +5,10 @@ function SobreMim () {
     return (
         <>
             <div className="container__sobremim">
-
+                <h1>Me chamo Lucas Ferreira Camilo</h1>
+                <h2>Comecei a minha tragetória profissional na  áre ade biologia, com pós-graduação em Análise ambiental. Em 2023 iniciei minha transição de carreira para a área de tecnologia.</h2>
+                <h2>Comecei na programação por python, mas me encatei pelo desenvolvimento web e decidi começar a estudar HTML, CSS E JavaScript. </h2>
+                <h2>Hoje, desenvolvo aplicaçoes web utilizando React.js, JavaScript, Node.js, e .NET com C#</h2>
             </div>
         </>
     )
